15ef345fd4a9f77786fdc8be2be4dabc58a1a307
eljuanorey <juanoterorey@tutanota.com>
2025-05-12 22:27:02 +0100

    initialize project with basic game setup and configuration files

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame, constants
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
