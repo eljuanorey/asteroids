9d8a648f7b2da852da75c93a5968ca343642ee9c
eljuanorey <juanoterorey@tutanota.com>
2025-06-14 11:25:28 +0100

    add player functionality and shape rendering; update constants and main game loop

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import pygame, constants
+import pygame, constants, player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player_instance = player.Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -10,7 +14,11 @@ def main():
                 return
             
         screen.fill((0, 0, 0))
+        player_instance.draw(screen)
+        player_instance.update(dt)
         pygame.display.flip()
+        
+        dt = clock.tick(constants.FPS) / 1000.0
 
 if __name__ == "__main__":
     main()
