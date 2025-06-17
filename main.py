ff2f8f3aae75a45908bbf5714471180cf3aa3070
eljuanorey <juanoterorey@tutanota.com>
2025-06-18 00:22:00 +0100

    implement player movement and update main game loop to handle drawable objects

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import pygame, constants, player
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    
+    #groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
 
     player_instance = player.Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
 
@@ -14,8 +21,11 @@ def main():
                 return
             
         screen.fill((0, 0, 0))
-        player_instance.draw(screen)
-        player_instance.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        updatable.update(dt)
         pygame.display.flip()
         
         dt = clock.tick(constants.FPS) / 1000.0
