e2371de4123175183ca86058dd47298429811d73
eljuanorey <juanoterorey@tutanota.com>
2025-06-22 20:23:54 +0100

    add asteroid and asteroid field classes; refactor main game loop for object management

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-import pygame, constants, player
+import pygame
+from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
     
     #groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
 
-    player_instance = player.Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+    player_instance = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
@@ -28,7 +35,7 @@ def main():
         updatable.update(dt)
         pygame.display.flip()
         
-        dt = clock.tick(constants.FPS) / 1000.0
+        dt = clock.tick(FPS) / 1000.0
 
 if __name__ == "__main__":
     main()
